d3f0d9df22594ae5c17036d85d62a65da26f1527
Eunice <eunicepatrina0@gmail.com>
2023-09-15 13:24:31 +0530

    add save Department

--- demo/src/main/java/com/example/demo/service/DeparmentServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/DeparmentServiceImpl.java
@@ -1,7 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Department;
+import com.example.demo.repository.DepartmentRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DeparmentServiceImpl implements DepartmentService{
+
+    private DepartmentRepository repository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        return repository.save(department);
+    }
 }
