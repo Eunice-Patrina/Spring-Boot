66e42c3b163293e6368885e2a03cc04bd8fe6631
Eunice <eunicepatrina0@gmail.com>
2023-09-19 12:35:27 +0530

    get department

--- demo/src/main/java/com/example/demo/Controller/DepartmentController.java
+++ demo/src/main/java/com/example/demo/Controller/DepartmentController.java
@@ -3,9 +3,12 @@ package com.example.demo.Controller;
 import com.example.demo.entity.Department;
 import com.example.demo.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -18,6 +21,11 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
     }
 
 }
--- demo/src/main/java/com/example/demo/service/DepartmentService.java
+++ demo/src/main/java/com/example/demo/service/DepartmentService.java
@@ -2,6 +2,10 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
 }
